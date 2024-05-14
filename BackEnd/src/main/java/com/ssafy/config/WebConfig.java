1ac83b2f381f827752a5438369561d563223f206
godeeyeon <godeeyeon@gmail.com>
2024-05-14 13:33:05 +0900

    Feat: Login form에서 로그인 요청(axios) 및 session에 userId 저장, WebConfig에 CORS 추가

--- BackEnd/src/main/java/com/ssafy/config/WebConfig.java
+++ BackEnd/src/main/java/com/ssafy/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,4 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 	}
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
--- vue-ssafy-final-pjt-k/src/stores/user.js
+++ vue-ssafy-final-pjt-k/src/stores/user.js
@@ -1,6 +1,32 @@
-import { ref, computed } from "vue";
+import router from "@/router";
+import axios from "axios";
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export const useUserStore = defineStore("user", () => {
-  return {};
+  // state
+  const loginUser = ref(null);
+  const USER_REST_API = "http://localhost:8080/api-user";
+
+  // actions
+  // 1) 로그인 요청
+  const submitLogin = (user) => {
+    axios({
+      url: `${USER_REST_API}/login`,
+      method: "POST",
+      data: user,
+    })
+      .then(() => {
+        loginUser.value = user.userId;
+        sessionStorage.setItem("loginUser", user.userId);
+        router.push({ name: "signup" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return {
+    loginUser,
+    submitLogin,
+  };
 });
