b23c9d0835a56d7a4a482b663dff19fff954efee
hi-react <godeeyeon@gmail.com>
2024-05-16 08:45:52 +0900

    Modify: Home 화면(로그인 여부에 따른 조건부 routing 처리 추가), HeaderNav(활성화 탭 조건 수정)

--- vue-ssafy-final-pjt-k/src/views/HomeView.vue
+++ vue-ssafy-final-pjt-k/src/views/HomeView.vue
@@ -1,13 +1,22 @@
 <template>
   <main id="background-image">
     <RouterLink to="/" id="home">Healing Forest</RouterLink>
-    <RouterLink :to="{ name: 'login' }" id="login"
-      >여정을 시작하시겠습니까?</RouterLink
-    >
+    <a @click="navigate" id="login">여정을 시작하시겠습니까?</a>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const navigate = () => {
+  if (sessionStorage.getItem("loginUser") != null) {
+    router.push({ name: "addictionList" });
+  } else {
+    router.push({ name: "login" });
+  }
+};
+</script>
 
 <style>
 #background-image {
@@ -51,5 +60,23 @@
   text-align: center;
   font-weight: 800;
   font-size: large;
+}
+
+/* media query */
+@media (max-width: 1024px) {
+  #home {
+    font-size: x-large;
+  }
+
+  #login {
+    font-size: medium;
+  }
+}
+
+@media (max-width: 768px) {
+  #login {
+    font-size: small;
+    padding: 1rem 3rem;
+  }
 }
 </style>
