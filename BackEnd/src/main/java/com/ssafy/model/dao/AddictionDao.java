1507626fbb76e6eaf9e816853811979b69711e92
godeeyeon <godeeyeon@gmail.com>
2024-05-20 01:31:43 +0900

    Modify: 상세 조회 - percentage링 이미지 및 목표 달성 시 문구 추가, addictionMapper.xml 수정
    - 전체조회: start_time 기준 최신순 정렬
    - 수정: 목표일자와 함께 end_time도 수정

--- BackEnd/src/main/java/com/ssafy/model/dao/AddictionDao.java
+++ BackEnd/src/main/java/com/ssafy/model/dao/AddictionDao.java
@@ -17,6 +17,7 @@ public interface AddictionDao {
 
 	// 상세 조회
 	public Addiction selectAddictionOne(@Param("addictionId") int addictionId);
+	
 	// 뱃지 조회
 	public List<Badge> selectBadge(int addictionId);
 
