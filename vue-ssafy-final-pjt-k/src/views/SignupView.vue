4650e3f3391c8c75c324706434720c1ccb151b2c
qkrrjsxor <qkrrjsxor12@gmail.com>
2024-05-21 18:03:02 +0900

    Feat: Signup Vue 유효성 검사, 백엔드 DB 연동

--- vue-ssafy-final-pjt-k/src/views/SignupView.vue
+++ vue-ssafy-final-pjt-k/src/views/SignupView.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="container">
-    <img
-      v-if="showImage"
-      id="login-image"
-      src="@/assets/signup-login.jpg"
-      alt="login-image"
-    />
+    <img v-if="showImage" id="login-image" src="@/assets/signup-login.jpg" alt="login-image" />
     <section id="login-section">
       <div id="login-form-box">
         <h2>Sign up</h2>
         <form id="login-form">
           <ul id="input-set">
             <li>
-              <input type="text" placeholder="아이디를 입력해주세요." />
-              <p></p>
+              <input type="text" placeholder="아이디를 입력해주세요." v-model="userId" @input="validateUserId" autofocus />
+              <p v-if="!isValidateId" class="validateAlert">아이디는 4~12자의 영문 대소문자와 숫자로 이루어져야 합니다.</p>
             </li>
             <li>
-              <input type="text" placeholder="비밀번호를 입력해주세요." />
-              <p></p>
+              <input type="password" placeholder="비밀번호를 입력해주세요." v-model="password" @input="validatePassword" />
+              <p v-if="!isValidatePassword" class="validateAlert">비밀번호는 8자 이상으로, 영문, 숫자, 특수문자를 모두 포함해야 합니다.</p>
             </li>
             <li>
-              <input type="text" placeholder="닉네임을 입력해주세요." />
-              <p></p>
+              <input type="text" placeholder="닉네임을 입력해주세요." :value="nickname" @input="setNickname" />
+              <p v-if="!isValidateNickname" class="validateAlert">닉네임은 2글자 이상이어야 합니다. </p>
             </li>
           </ul>
           <div id="button-set">
             <button @click="goHome">홈으로</button>
-            <button>회원가입</button>
+            <button type="button" @click.prevent="submitSignup">회원가입</button>
           </div>
           <RouterLink :to="{ name: 'login' }">이미 회원이신가요?</RouterLink>
         </form>
@@ -38,8 +33,10 @@
 <script setup>
 import { ref, watchEffect } from "vue";
 import { useRouter } from "vue-router";
+import { useUserStore } from "@/stores/user";
 
 const showImage = ref(true);
+const store = useUserStore();
 
 // [반응형] 화면 리사이징 -> image 제거
 watchEffect(() => {
@@ -60,6 +57,49 @@ const router = useRouter();
 // 홈으로
 const goHome = () => {
   router.push({ name: "home" });
+};
+
+// 정규식 조건
+const userIdPattern = /^[a-zA-Z0-9]{4,12}$/;  // 4~12 글자 사이, 영어 소문자, 대문자, 숫자만 가능
+const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; //8글자 이상, 영대문자, 소문자, 숫자, 특수문자 포함
+const nicknamePattern = /^.{2,}$/;
+
+const userId = ref('');
+const password = ref('');
+const nickname = ref('');
+
+const isValidateId = ref(true);
+const isValidatePassword = ref(true);
+const isValidateNickname = ref(true);
+
+const validateUserId = () => {
+  isValidateId.value = userIdPattern.test(userId.value) ? true : false;
+  // console.log(isValidateId.value)
+}
+const validatePassword = () => {
+  isValidatePassword.value = passwordPattern.test(password.value) ? true : false;
+  // console.log(isValidatePassword.value)
+};
+const validateNickname = (nickname) => {
+
+  isValidateNickname.value = nicknamePattern.test(nickname) ? true : false;
+  // console.log(isValidateNickname.value)
+  // console.log(nickname)
+};
+
+const setNickname = (event) => {
+  nickname.value = event.target.value;
+  validateNickname(nickname.value)
+}
+
+// 회원가입 요청 submit
+const submitSignup = () => {
+  if (isValidateId.value && isValidatePassword.value && isValidateNickname.value && userId.value.trim() !== '') {
+    console.log('submit')
+    store.submitSignup(userId.value, password.value, nickname.value);
+  } else {
+    console.log('submit fail')
+  }
 };
 </script>
 
@@ -154,6 +194,20 @@ a {
   color: #9d8e03;
   font-size: small;
   font-weight: 700;
+}
+
+.validateAlert {
+  font-size: small;
+  font-weight: 700;
+  color: red;
+}
+
+#alert {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -150%);
+  width: 85%;
 }
 
 /* media query */
